3fbe9ccb38d1d91dba046fc47004763c51cd3684
Cole Bambrick <kcirbmab@gmail.com>
2013-03-09 17:39:55 -0500

    Added particle effect when player gets a healthup

--- explosion.lua
+++ explosion.lua
@@ -19,15 +19,15 @@
 --]]----------------------------------------------------------------------------
 
 require "lib.classlib"
-
+class.HealthUp()
 class.Explosion()
 
-local explosionParticle = love.graphics.newImage("res/explosion.png");
+local particle = love.graphics.newImage("res/explosion.png");
 
 function Explosion:__init(x, y, angle)
 	self.timer = 0
 	scene:addObject(self)
-	self.p = love.graphics.newParticleSystem(explosionParticle, 1000)
+	self.p = love.graphics.newParticleSystem(particle, 1000)
 	self.p:setEmissionRate(100)
 	self.p:setSpeed(100, 1000)
 	self.p:setSizes(1, 1)
@@ -56,16 +56,32 @@ end
 
 --------------------------------------------------------------------------------
 
-class.ExplosionChunk()
-
-function ExplosionChunk:__init(x, y)
-	
+function HealthUp:__init(x, y, angle)
+	self.timer = 0
+	scene:addObject(self)
+	self.p = love.graphics.newParticleSystem(particle, 1000)
+	self.p:setEmissionRate(100)
+	self.p:setSpeed(100, 1000)
+	self.p:setSizes(1, 1)
+	self.p:setColors(255, 255, 255, 255, 50, 255, 50, 0)
+	self.p:setPosition(400, 300)
+	self.p:setLifetime(0.1)
+	self.p:setParticleLife(0.3)
+	self.p:setDirection(0)
+	self.p:setSpread(2*math.pi)
+	self.p:setPosition(x,y)
+	self.p:setDirection(angle-math.pi)
 end
 
-function ExplosionChunk:update(dt)
-	
+function HealthUp:update(dt)
+	self.timer = self.timer + dt
+	self.p:update(dt)
+	if self.timer > 1 then
+		self.p:stop()
+		scene:removeObject(self)
+	end
 end
 
-function ExplosionChunk:draw()
-	
+function HealthUp:draw()
+	love.graphics.draw(self.p, 0, 0)
 end
--- goal.lua
+++ goal.lua
@@ -20,6 +20,7 @@
 
 require "lib.classlib"
 require "radar"
+require "explosion"
 
 class.Goal()
 
@@ -33,7 +34,14 @@ function Goal:__init(parent, x, y, x1, y1, x2, y2)
 
 	-- Initialize properties
 	self.radar = RadarBlip(parent:getRadar(), self, 16, COLOR_LIME)
-	self.radius = 2000
+	self.radius = 2000	-- amount the goal can move when respawning
+	
+	-- pulse (changing the size of the goal) parameters
+	self.outerSize = { min = 4, max = 32, curr = 16 }
+	self.innerSize = { min = 6,  max = 12, curr = 8  }
+	self.pulseDir = 1
+	self.timer = 0
+	self.interval = 1
 	
 	-- Initialize physics and collision detection
 	local width			= 120
@@ -53,7 +61,20 @@ end
 -- Update
 --==============================================================================
 function Goal:update(dt)
+	-- update timer
+	if self.pulseDir == 1 then self.timer = self.timer + dt
+	elseif self.pulseDir == 0 then self.timer = self.timer - dt end
 	
+	-- resize goal
+	local innerDifference = self.innerSize.max - self.innerSize.min
+	local outerDifference = self.outerSize.max - self.outerSize.min
+	local timerAdjustment = self.timer * ( 1 / self.interval )
+	self.innerSize.curr = self.innerSize.min + timerAdjustment * innerDifference
+	self.outerSize.curr = self.outerSize.min + timerAdjustment * outerDifference
+	
+	-- determine if the goal should expand or contract
+	if self.timer > self.interval then self.pulseDir = 0 end
+	if self.timer < 0 then self.pulseDir = 1 end
 end
 
 --==============================================================================
@@ -64,9 +85,9 @@ function Goal:draw()
 	love.graphics.translate(self.body.x, self.body.y)
 	love.graphics.rotate(self.body.angle + math.pi/2)
 	love.graphics.setColor(COLOR_LIME)
-	love.graphics.circle('line', 0, 0, 16)
-	love.graphics.circle('line', 0, 0, 8)
-	love.graphics.circle('fill', 0, 0, 8)
+	love.graphics.circle('line', 0, 0, self.outerSize.curr)
+	love.graphics.circle('line', 0, 0, self.innerSize.curr)
+	love.graphics.circle('fill', 0, 0, self.innerSize.curr)
 	love.graphics.pop()
 end
 
@@ -79,6 +100,9 @@ function Goal:capture()
 	local a		= player.body.angle - math.pi/4
 	local b		= player.body.angle + math.pi/4
 	local angle = math.random(a,b) + math.pi
+	
+	-- create particle effect
+	HealthUp(self.body.x, self.body.y, 0)
 	
 	-- move to new area
 	self.body.x = player:getX() - (self.radius * math.cos(angle))
